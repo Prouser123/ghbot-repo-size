8d51629f4b1e7952b1bc62d330b1b689c1f9ca9a
James Cahill <jcahill773@gmail.com>
2019-10-31 19:43:46 +0000

    PoC 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const rs = require("reposize");
+const pretty = require("pretty-bytes");
 
 /**
  * This is the main entrypoint to your Probot app
@@ -11,22 +12,41 @@ module.exports = app => {
   app.on("pull_request", async context => {
     const pr = context.payload.pull_request;
     const sha = pr.head.sha;
+    const base_sha = pr.base.sha;
 
     const params = context.repo();
 
+    const base_rs = await rs.withKit.GetBytesFromSha(
+      context.github,
+      params.owner,
+      params.repo,
+      base_sha
+    );
+
+    const head_rs = await rs.withKit.GetBytesFromSha(
+      context.github,
+      params.owner,
+      params.repo,
+      sha
+    );
+
     const status = {
       sha,
-      context: "repo/size",
+      context: "size/diff",
       state: "success",
-      description: await rs.withKit.GetPrettyBytesFromSha(
-        context.github,
-        params.owner,
-        params.repo,
-        sha
-      )
+      description: pretty(head_rs - base_rs, { signed: true })
+    };
+
+    const status2 = {
+      sha,
+      context: "size/size",
+      state: "success",
+      description: `${pretty(head_rs)} (was ${pretty(base_rs)})`
     };
 
     // Create the status
-    return context.github.repos.createStatus(context.repo(status));
+    context.github.repos.createStatus(context.repo(status));
+    context.github.repos.createStatus(context.repo(status2));
+    return;
   });
 };
