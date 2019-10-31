2308265c2b0415f5649bfd17c224151f7077dd69
James Cahill <jcahill773@gmail.com>
2019-10-31 20:22:39 +0000

    Release 1.0

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const rs = require("reposize");
 const pretty = require("pretty-bytes");
 
+function createChecks({ context, params, head_sha, head_rs, base_rs, isPR }) {
+  // Create the checks
+  context.github.checks.create({
+    owner: params.owner,
+    repo: params.repo,
+    name: "size/diff",
+    head_sha,
+    status: "completed",
+    conclusion: "neutral",
+    output: {
+      title: `[${pretty(head_rs - base_rs, { signed: true })}]`,
+      summary: isPR
+        ? "This is the difference in file size between the HEAD of this branch and the base branch. (the one you are merging into)"
+        : "This is the difference in file size between this commit and the previous commit."
+    }
+  });
+
+  context.github.checks.create({
+    owner: params.owner,
+    repo: params.repo,
+    name: "size/size",
+    head_sha,
+    status: "completed",
+    conclusion: "neutral",
+    output: {
+      title: `${pretty(head_rs)} (was ${pretty(base_rs)})`,
+      summary: "This is the new file size of your repository after this commit."
+    }
+  });
+}
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -8,6 +39,37 @@ const pretty = require("pretty-bytes");
 module.exports = app => {
   // Your code here
   app.log("Application loaded!");
+
+  app.on("push", async context => {
+    const base_sha = context.payload.before;
+    const sha = context.payload.after;
+    const params = context.repo();
+
+    if (base_sha != "0000000000000000000000000000000000000000") {
+      const base_rs = await rs.withKit.GetBytesFromSha(
+        context.github,
+        params.owner,
+        params.repo,
+        base_sha
+      );
+
+      const head_rs = await rs.withKit.GetBytesFromSha(
+        context.github,
+        params.owner,
+        params.repo,
+        sha
+      );
+
+      createChecks({
+        context,
+        params,
+        head_sha: sha,
+        head_rs,
+        base_rs,
+        isPR: false
+      });
+    }
+  });
 
   app.on("pull_request", async context => {
     const pr = context.payload.pull_request;
@@ -30,33 +92,13 @@ module.exports = app => {
       sha
     );
 
-    // Create the checks
-    context.github.checks.create({
-      owner: params.owner,
-      repo: params.repo,
-      name: "size/diff",
+    createChecks({
+      context,
+      params,
       head_sha: sha,
-      status: "completed",
-      conclusion: "neutral",
-      output: {
-        title: `[${pretty(head_rs - base_rs, { signed: true })}]`,
-        summary:
-          "This is the difference in file size between the HEAD of this branch and the base branch. (the one you are merging into)"
-      }
-    });
-
-    context.github.checks.create({
-      owner: params.owner,
-      repo: params.repo,
-      name: "size/size",
-      head_sha: sha,
-      status: "completed",
-      conclusion: "neutral",
-      output: {
-        title: `${pretty(head_rs)} (was ${pretty(base_rs)})`,
-        summary:
-          "This is the new file size of your repository after this commit."
-      }
+      head_rs,
+      base_rs,
+      isPR: true
     });
     return;
   });
